06b870c381447aa8f5b4c809fea9361f7da00ffe
Tamanna Jain <tj2358@nyu.edu>
2024-04-18 21:34:32 -0400

    finals

--- NewYork/index.js
+++ NewYork/index.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'))
+
+app.use(express.json({ limit: '1mb' }));
+app.use(express.static('public'));
+
+app.listen(3001, () => console.log('Listening at port 3001'));
+
+// Define other routes as needed
+app.post('/api', (request, response) => {
+    console.log(request.body);
+    response.json({
+        message: "Data received at /api",
+        email: request.body.email
+    });
+});
